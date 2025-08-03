c89c13865d7b15ca8ff66336f7b89e2aa51af914
enzosoaresdev <enzo.ferreira.soares@hotmail.com>
2025-08-03 10:11:57 -0300

    feat: Estrutura pré finalizada do código

--- diagnostic_bot.py
+++ diagnostic_bot.py
@@ -5,7 +5,21 @@ perguntas = [
     }
 ]
 
-for pergunta in perguntas:
-    comando = input(f'{pergunta["pergunta"]} (s/n): ')
-    if comando.lower() == "s":
-        print(f'{pergunta["solucao"]}')
+
+while True:
+    resposta = False
+    for pergunta in perguntas:
+        comando = input(f'{pergunta["pergunta"]} (s/n): ')
+        if comando.lower().strip() == "s":
+            print(f'{pergunta["solucao"]}')
+            resposta = True
+            break
+
+    if not resposta:
+        nova_solucao = input("Desisto. Qual era a solução para seu problema? ")
+        nova_pergunta = input("Qual dúvida de 'sim/não' levaria a essa solução? ")
+        perguntas.append({"pergunta": nova_pergunta, "solucao": nova_solucao})
+
+    resposta = input("Gostaria de tentar novamente? (s/n): ")
+    if resposta.lower().strip() != "s":
+        break
